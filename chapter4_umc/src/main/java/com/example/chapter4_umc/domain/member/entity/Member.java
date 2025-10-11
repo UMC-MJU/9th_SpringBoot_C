a724b0eda135c2d853e9b22e6c2cf8f1e431d60d
Sejeong Kim <sejeonggim68@gmail.com>
2025-10-12 00:59:11 +0900

    feat: Region 엔티티 생성 및 ê1:N 관계 매핑

---
+++ chapter4_umc/src/main/java/com/example/chapter4_umc/domain/member/entity/Member.java
@@ -0,0 +1,1 @@
+package com.example.chapter4_umc.domain.member.entity;
---
+++ chapter4_umc/src/main/java/com/example/chapter4_umc/domain/region/entity/Region.java
@@ -0,0 +1,37 @@
+package com.example.chapter4_umc.domain.region.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.apache.catalina.Store;
+
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "region")
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "region_id")
+    private Long id;
+
+    @Column(name = "region_name", length = 100) // 지역 이름 (VARCHAR 100)
+    private String regionName;
+
+    // 1:N 관계: Region (1) : Store (N) (지역 하나마다 여러 가게 존재)
+    @OneToMany(mappedBy = "region", cascade = CascadeType.ALL)
+    private List<Store> storeList;
+
+    // 1:N 관계: Region (1) : Member (N) (여러 사용자가 하나의 지역을 선호 지역으로 설정 가능)
+    @OneToMany(mappedBy = "preferredRegion", cascade = CascadeType.ALL)
+    private List<Member> MemberList;
+
+    // 1:N 관계: Region (1) : Review (N) (한 지역에 리뷰 여러 개가 등록될 수 있음)
+    @OneToMany(mappedBy = "region", cascade = CascadeType.ALL)
+    private List<Review> reviewList;
+
+}
